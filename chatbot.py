df1484ba69376b63f0d77681de92d3e2f1e226ed
Jack Carter <jackpcarter.dev@gmail.com>
2026-07-30 21:41:09 +0100

    feat: add notion retrieval, parsing and tf-idf vectorisation logic

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,30 @@
-def chatbot(message):
-    return message
+import os
+from dotenv import load_dotenv
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+from update_data import update_data
+
+load_dotenv()
+
+NOTION_API_KEY = os.getenv('NOTION_API_KEY')
+NOTION_PAGE_ID = os.getenv('NOTION_PAGE_ID')
+CHUNKS_USED = int(os.getenv('CHUNKS_USED'))
+
+documents, tfidf_vectorizer, tfidf_matrix = update_data(NOTION_API_KEY, NOTION_PAGE_ID)
+
+def chatbot(query: str) -> str:
+    """
+    Transforms the user query into tf-idf vectors and uses 
+    cosine similarity to find the k best context chunks for
+    an LLM to answer the question. 
+    """
+    query_vector = tfidf_vectorizer.transform([query])
+
+    scores = cosine_similarity(query_vector, tfidf_matrix)[0]
+    best = scores.argsort()[::-1][:CHUNKS_USED]
+
+    best_k_chunks = [documents[num] for num in best]
+    context = '\n\n'.join(best_k_chunks)
+
+    return context
---
+++ parse_notion_text.py
@@ -0,0 +1,111 @@
+def is_heading(block, block_type):
+
+    headings = [
+        'heading_1',
+        'heading_2',
+        'heading_3',
+        'heading_4',
+    ]
+
+    if block_type in headings:
+        rich_text = block.get(block_type, {}).get('rich_text', [])
+        return bool(rich_text)
+
+    if block_type == 'paragraph':
+
+        rich_text = block.get("paragraph", {}).get("rich_text", [])
+
+        if not rich_text:
+            return False
+
+        text = ''.join(
+            item.get('plain_text', '')
+            for item in rich_text
+        )
+
+        is_bold = all(
+            item.get('annotations', {}).get('bold', False)
+            for item in rich_text
+        )
+
+        is_short = len(text.split()) <= 10
+
+        if is_bold and is_short:
+            return True
+
+    return False
+
+
+def parse_notion_text(data):
+
+    current_topic = ''
+    last_topic = ''
+    current_text = ''
+    last_heading = False
+    started = False
+    chunks = []
+
+    for block in data.get('results', []):
+
+        block_type = block.get('type')
+
+        if is_heading(block, block_type):
+
+            if current_text:
+                chunks.append({
+                    'topic': current_topic.strip(),
+                    'text': current_text.strip()
+                })
+                last_topic = current_topic
+                current_topic = ''
+                current_text = ''
+                started = True
+
+            heading_data = block.get(block_type, {})
+
+            text = ''.join(
+                t.get('plain_text', '')
+                for t in heading_data.get('rich_text', [])
+            ).strip()
+
+            if started and last_heading:
+                current_topic = current_topic.split(' > ')[1].strip() + ' > ' + text.strip()
+            else:
+                if current_topic:
+                    current_topic = current_topic.strip() + ' > ' + text.strip()
+                else:
+                    if last_topic:
+                        current_topic = last_topic.split(' > ')[0].strip() + ' > ' + text.strip()
+                    else:
+                        current_topic = text.strip()
+
+            last_heading = True
+
+
+        if block_type == 'paragraph':
+
+            last_heading = False
+
+            text = ''.join(
+                t['plain_text'] for t in block['paragraph']['rich_text']
+            )
+
+            if len(text.split()) <= 0:
+                pass
+
+            if not current_text:
+                current_text = text.strip()
+            else:
+                current_text = current_text.strip() + ' ' + text.strip()
+
+    chunks.append({
+        'topic': current_topic.strip(),
+        'text': current_text.strip()
+    })
+
+    return chunks
+
+    
+
+
+
